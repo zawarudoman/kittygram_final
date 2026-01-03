df472758f4d7042cebbb3870fb49e798ff40bf2e
Максим Лесков <lml@MacBook-Air-Maksim.local>
2026-01-03 16:39:09 +0300

    fix5

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -96,7 +96,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/staticfiles/static'
+STATIC_ROOT = '/staticfiles/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
